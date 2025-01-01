7de5ff46e6efbeffe570fabd21e92202fb645b84
Matt Vogel <mainmoniker@googlemail.com>
2025-01-01 13:25:23 -0500

    auto-update footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,8 +1,10 @@
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+  
   return (
     <footer className="bg-gray-900 text-white py-12">
       <div className="border-t border-gray-800 mt-12 pt-6 text-center text-gray-400">
-          © 2024 Matt Vogel. All rights reserved.
+        © {currentYear} Matt Vogel. All rights reserved.
         </div>
       {/* <div className="container mx-auto px-4"> */}
         {/* <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
